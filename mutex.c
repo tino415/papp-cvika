a225b1cfad03209b054cbb33046e35a390bfa288
Martin Cernak <xcernak@stuba.sk>
2014-10-09 10:07:12 +0200

    Added mutex sample

---
+++ mutex.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+//TODO make it done
+
+typedef struct mthread {
+	int id;
+	char message[50];
+	pthread_t thread;
+} MThread;
+
+pthread_mutex_t condition_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *thread_function (void *arg) {
+	MThread *thread = arg;
+	int i;
+	Sleep(rand());
+
+	pthread_mutex_lock( &condition_mutex );
+
+	for(i = 0; i < 3; i++) {
+	    Sleep(1);
+	    printf("%d in critical place\n", thread->id);
+	}
+
+	pthread_mutex_unlock( &condition_mutex );
+
+	return 0;
+}
+
+int main(void) {
+	int id;
+	int num_of_threads = -1;
+
+	srand(time(NULL));
+
+	while(num_of_threads < 0 || num_of_threads > 6) {
+		printf("Zadajte pocet threadow(od 0 do 5):\n");
+		scanf("%d", &num_of_threads);
+	}
+
+	MThread *threads = malloc(num_of_threads*sizeof(MThread));
+
+	for(id = 0; id < num_of_threads; id++) {
+		threads[id].id = id;
+		pthread_create(&(threads[id].thread), NULL, &thread_function, (void*) &(threads[id]));
+	}
+
+	for(id = 0; id < num_of_threads; id++) {
+		pthread_join(threads[id].thread, NULL);
+	}
+
+	free(threads);
+	return 0;
+}
